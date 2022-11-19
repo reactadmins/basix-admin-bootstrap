2ac43bfac129063eeb9670bbff22f3a31064e2ff
Dipok Roy <84003272+Dip022@users.noreply.github.com>
2022-11-19 20:25:57 +0600

    error page design update

--- src/pages/Error404/Error404.js
+++ src/pages/Error404/Error404.js
@@ -1,23 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import logo from "../../images/logo.png";
 import "./Error404.scss";
 
 const Error404 = () => {
   return (
     <div className="container-fluid p-0">
-      <div className="error-wrapper d-flex justify-content-center align-items-center">
-        <div className="position-relative text-center">
-          <h1 className="display-5 fw-bold text-white">Oops!</h1>
-          <span className="text-white fs-5 fw-bold">404 PAGE NOT FOUND</span>
-          <p className="text-white">
-            The page you are looking for was not found.
-          </p>
-          <Link
-            to="/dashboard"
-            className="btn btn-primary text-decoration-none"
-          >
-            Go to Dashboard
-          </Link>
+      <div className="container-fluid p-0">
+        <div className="error404-wrapper">
+          <div className="error-container bg-white h-100">
+            <div className="">
+              <div style={{ marginLeft: "20px" }}>
+                <h1 className="error-code">404</h1>
+                <h5 className="error-title">Oops! You're lost.</h5>
+                <p className="error-description">
+                  The page you are looking <br /> for was not found.
+                </p>
+              </div>
+              <div class="input-group">
+                <button type="button" className="error-search-btn">
+                  <i className="fa fa-search"></i>
+                </button>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="What are you looking for?"
+                  required
+                />
+                <button type="submit" className="error-submit-btn">
+                  Search
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="logo-container h-100">
+            <div className="oblique"></div>
+            <div className="logo">
+              <a href="">
+                <img src={logo} alt="logo" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
