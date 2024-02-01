a4aec9a52255643fc3be915c85c64dbf1781ccb1
Dipok Roy <roydipok022@gmail.com>
2024-02-01 16:14:23 +0600

    leaflet maps added

--- src/views/LeafletMaps.jsx
+++ src/views/LeafletMaps.jsx
@@ -1,34 +1,57 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import CardContent from "../components/Card/CardContent";
+import PopupWithMarker from "../components/Maps/LeafletMaps/PopupWithMarker";
+import { Col, Container, Row } from "react-bootstrap";
+import VectorLayers from "../components/Maps/LeafletMaps/VectorLayers";
+import LayersControlMaps from "../components/Maps/LeafletMaps/LayersControlMaps";
+import LeafletSVGOverlay from "../components/Maps/LeafletMaps/LeafletSVGOverlay";
 
 const LeafletMaps = () => {
-    const position = [51.505, -0.09];
-
     return (
-        <CardContent
-            title="Leaflet Maps"
-            CardBody={
-                <>
-                    <MapContainer
-                        style={{ width: "100%", height: "100vh" }}
-                        center={position}
-                        zoom={13}
-                        scrollWheelZoom={false}
-                    >
-                        <TileLayer
-                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                        <Marker position={position}>
-                            <Popup>
-                                A pretty CSS3 popup. <br /> Easily customizable.
-                            </Popup>
-                        </Marker>
-                    </MapContainer>
-                </>
-            }
-        />
+        <Container fluid className="p-0">
+            <Row className="gy-4 gx-4 mb-4">
+                <Col col={6} lg={6} xl={6}>
+                    <CardContent
+                        title="Popup with Marker"
+                        CardBody={
+                            <>
+                                <PopupWithMarker />
+                            </>
+                        }
+                    />
+                </Col>
+                <Col col={6} lg={6} xl={6}>
+                    <CardContent
+                        title="Vector layers"
+                        CardBody={
+                            <>
+                                <VectorLayers />
+                            </>
+                        }
+                    />
+                </Col>
+                <Col col={6} lg={6} xl={6}>
+                    <CardContent
+                        title="Layers control Maps"
+                        CardBody={
+                            <>
+                                <LayersControlMaps />
+                            </>
+                        }
+                    />
+                </Col>
+                <Col col={6} lg={6} xl={6}>
+                    <CardContent
+                        title="Leaflet SVG Overlay"
+                        CardBody={
+                            <>
+                                <LeafletSVGOverlay />
+                            </>
+                        }
+                    />
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
