34955ca404a1fae12ed2819b0f4692606635af51
kakonbarman <labudiu@gmail.com>
2024-02-12 11:56:32 +0600

    Badge Stories add

--- docs/stories/Badge.stories.js
+++ docs/stories/Badge.stories.js
@@ -3,4 +3,42 @@ import { Badge } from "react-bootstrap";
 export default {
     title: "Components/Badge",
     component: Badge,
+    tags: ["autodocs"],
+    argTypes: {
+        bg: {
+            control: "select",
+            options: [
+                "primary",
+                "secondary",
+                "success",
+                "danger",
+                "waring",
+                "info",
+                "light",
+                "dark",
+            ],
+        },
+        text: {
+            control: "select",
+            options: [
+                "primary",
+                "secondary",
+                "success",
+                "danger",
+                "waring",
+                "info",
+                "light",
+                "dark",
+            ],
+        },
+    },
+};
+
+export const Normal = {
+    args: {
+        bg: "primary",
+        children: "Primary",
+        pill: false,
+        text: "",
+    },
 };
