a6fad8f427a3d2cecf27cea12171a45cebb68342
kakonbarman <kakonbarman4366@gmail.com>
2024-02-17 00:00:28 +0600

    working

--- docs/static/theme-mode.js
+++ docs/static/theme-mode.js
@@ -8,6 +8,28 @@ if (ExecutionEnvironment.canUseDOM) {
         const darkBtn = document.querySelector(".light-dark-dropdown .dark");
         const systemBtn = document.querySelector(".light-dark-dropdown .system");
 
+        const dropdown = document.querySelector(".light-dark-dropdown");
+        const modeIcon = document.querySelector(".mode-icon");
+
+        document.addEventListener('click', (event) => {  
+            const isVisible  = getComputedStyle(dropdown).visibility === "visible";       
+            console.log("isVisible", isVisible);
+
+            if (!dropdown.contains(event.target)) {
+                if(!!isVisible){
+                    dropdown.style.visibility = 'hidden';
+                }
+            }
+        });
+
+        handleClick(modeIcon, () => {
+            dropdown.style.visibility = "visible";
+        })
+
+        // console.log("dropdown", dropdown.style)
+        // console.log("style", getComputedStyle(dropdown).visibility);
+
+
         const mode = localStorage.getItem("theme");
 
         if(mode === "dark"){
@@ -50,5 +72,9 @@ function handleMode(mode){
     localStorage.setItem("theme", mode);
     document.documentElement.setAttribute('data-theme', mode);
     document.documentElement.setAttribute('data-bs-theme', mode);
+
+    const dropdown = document.querySelector(".light-dark-dropdown");
+
+    dropdown.style.visibility = "hidden";
 }
 
