1cf02ed219178dcf884d01f27316ad64af15aa80
Dipok Roy <roydipok022@gmail.com>
2024-02-17 18:49:58 +0600

    modals update

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,87 @@
+const Modal = ({ label, modalId, title, size, children }) => {
+    return (
+        <>
+            <button
+                type="button"
+                className="btn btn-secondary m-1"
+                data-bs-toggle="modal"
+                data-bs-target={`#${modalId}`}
+                data-bs-whatever="@mdo"
+            >
+                {label}
+            </button>
+            <div
+                className="modal fade"
+                id={`${modalId}`}
+                tabIndex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
+                <div className={`modal-dialog ${size}`}>
+                    <div
+                        className="modal-content"
+                        style={{
+                            backgroundColor: "var(--bg-content)",
+                            border: "1px solid var(--border-color)",
+                        }}
+                    >
+                        <div
+                            className="modal-header"
+                            style={{ borderColor: "var(--border-color)" }}
+                        >
+                            <h5
+                                className="modal-title"
+                                id="exampleModalLabel"
+                                style={{ color: "var(--hedging-text-color)" }}
+                            >
+                                {title}
+                            </h5>
+                            <button
+                                type="button"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            >
+                                <i
+                                    className="fa-solid fa-xmark"
+                                    style={{
+                                        fontSize: "20px",
+                                        color: "var(--hedging-text-color)",
+                                    }}
+                                ></i>
+                            </button>
+                        </div>
+                        <div
+                            className="modal-body"
+                            style={{
+                                color: "var(--hedging-text-color)",
+                            }}
+                        >
+                            {children}
+                        </div>
+                        <div
+                            className="modal-footer"
+                            style={{ borderColor: "var(--border-color)" }}
+                        >
+                            <button
+                                type="button"
+                                className="btn btn-primary"
+                                data-bs-dismiss="modal"
+                            >
+                                CONFIRM
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                data-bs-dismiss="modal"
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Modal;
--- src/views/Modals.jsx
+++ src/views/Modals.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Card from "../components/Card/Card";
-import { Button, CardBody, Col, Container, Modal, Row } from "react-bootstrap";
+import { CardBody, Col, Container , Row} from "react-bootstrap";
+import Modal from "../components/Modal/Modal";
 
 const Modals = () => {
     const [show, setShow] = useState(false);
@@ -10,29 +11,67 @@ const Modals = () => {
     return (
         <Container fluid className="p-0">
             <Row>
-                <Col sm={12} md={12} lg={12} xl={7}>
+                <Col sm={12}>
                     <Card title="Modals">
-                        <CardBody>
-                            <div>
-                                <Button variant="primary" onClick={handleShow}>Small</Button>
-                                <Modal show={show} onHide={handleClose}>
-                                    <Modal.Header closeButton>
-                                    <Modal.Title>Modal heading</Modal.Title>
-                                    </Modal.Header>
-                                    <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-                                    <Modal.Footer>
-                                    <Button variant="secondary" onClick={handleClose}>
-                                        Close
-                                    </Button>
-                                    <Button variant="primary" onClick={handleClose}>
-                                        Save Changes
-                                    </Button>
-                                    </Modal.Footer>
-                                </Modal> 
-                            </div> 
-                            <div>
-                            
-                            </div>            
+                    <CardBody>
+                            <Modal
+                                label="Small"
+                                modalId="small"
+                                title="Small modal"
+                                size="modal-sm"
+                            >
+                                <p>There are three species of zebras: the plains zebra, the mountain zebra and the Grévy's zebra. The plains zebra and the mountain zebra belong to the subgenus Hippotigris, but Grévy's zebra is the sole species of subgenus Dolichohippus. The latter resembles an ass, to which it is closely related, while the former two are more horse-like. All three belong to the genus Equus, along with other living equids</p>
+                            </Modal>
+                            <Modal
+                                label="Medium"
+                                modalId="medium"
+                                title="Medium modal"
+                                size="modal-md"
+                            >
+                                <p>There are three species of zebras: the plains zebra, the mountain zebra and the Grévy's zebra. The plains zebra and the mountain zebra belong to the subgenus Hippotigris, but Grévy's zebra is the sole species of subgenus Dolichohippus. The latter resembles an ass, to which it is closely related, while the former two are more horse-like. All three belong to the genus Equus, along with other living equids.</p>
+                            </Modal>
+                            <Modal
+                                label="Large"
+                                modalId="large"
+                                title="Large Modal"
+                                size="modal-lg"
+                            >
+                                <p>There are three species of zebras: the plains zebra, the mountain zebra and the Grévy's zebra. The plains zebra and the mountain zebra belong to the subgenus Hippotigris, but Grévy's zebra is the sole species of subgenus Dolichohippus. The latter resembles an ass, to which it is closely related, while the former two are more horse-like. All three belong to the genus Equus, along with other living equids.</p>
+                            </Modal>
+                            <Modal
+                                label="Scrolling"
+                                modalId="scrolling"
+                                title="Scrolling Long Content Modal"
+                                size="modal-lg"
+                               
+                            >
+                                <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                                Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+                                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                                Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+                                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                                Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+                                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                                Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+                                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                                Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.
+                                Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                                Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+                            </Modal>
+                            <Modal
+                                label="Static"
+                                modalId="static"
+                                title="Static Modal"
+                                size="modal-sm"
+                            >
+                                <p>This is a static modal, backdrop click will not close it.</p>
+                            </Modal>
                         </CardBody>
                     </Card>
                 </Col>
