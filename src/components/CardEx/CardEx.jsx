bd452a10e92d6d40d897bcdb5a0f2ad74d0db554
kakonbarman <kakonbarman4366@gmail.com>
2024-02-18 15:26:39 +0600

    cleanup

--- src/components/CardEx/CardEx.jsx
+++ src/components/CardEx/CardEx.jsx
@@ -19,7 +19,6 @@ const CardEx = ({
     children = "",
 }) => {
     const [isOpen, setIsOpen] = useState(false);
-    const [hiddenContent, setHiddenContent] = useState(false);
 
     const ref = useRef();
 
@@ -34,84 +33,78 @@ const CardEx = ({
 
     return (
         <Fragment>
-            {!hiddenContent ? (
-                <BootstrapCard className={`${CardStyle.card} rounded-0 w-100 h-100`}>
-                    {headerPosition === "bottom" ? (
-                        <div style={{ padding: padding, background: bodyBg }}>{children}</div>
-                    ) : null}
+            <BootstrapCard className={`${CardStyle.card} rounded-0 w-100 h-100`}>
+                {headerPosition === "bottom" ? (
+                    <div style={{ padding: padding, background: bodyBg }}>{children}</div>
+                ) : null}
 
-                    {!!isHeader ? (
-                        <CardHeader
-                            className={`${CardStyle.card_header} rounded-0`}
-                            {...(titleBg ? { style: { background: titleBg } } : "")}>
-                            <div
-                                className={`d-flex align-items-center ${
-                                    icons.length > 0 ? "justify-content-between" : ""
-                                }`}>
-                                <div>
-                                    {titleIcon ? (
-                                        <span className={classNames(styles.titleIcon, titleIcon)} />
-                                    ) : null}
-                                    <strong className={CardStyle.card_title}>{title}</strong>
-                                    {subscript}
-                                </div>
-                                {icons.length > 0 && (
-                                    <div
-                                        className={`d-flex align-items-center ${CardStyle.card_icon}`}>
-                                        {icons?.map((item, index) => (
-                                            <Fragment key={index}>
-                                                <button
-                                                    type="button"
-                                                    onClick={() => {
-                                                        setIsOpen(
-                                                            item.icon === "fa fa-cog"
-                                                                ? !isOpen
-                                                                : "" ||
-                                                                  item.icon === "fa fa-ellipsis-v"
-                                                                ? !isOpen
-                                                                : ""
-                                                        );
-                                                    }}
-                                                    className={`${CardStyle.content_settings} border-0 bg-transparent`}>
-                                                    <i className={item.icon} />
-                                                </button>
-
-                                                {isOpen && item?.dropdown?.length > 0 ? (
-                                                    <div
-                                                        ref={ref}
-                                                        className={CardStyle.card_setting}>
-                                                        {item?.dropdown?.map((menu, i) => (
-                                                            <button
-                                                                key={i}
-                                                                type="button"
-                                                                onClick={menu?.method}>
-                                                                <i className={menu?.icon} />
-                                                                {menu.label}
-                                                            </button>
-                                                        ))}
-                                                    </div>
-                                                ) : null}
-                                            </Fragment>
-                                        ))}
-                                        {!!dismissible ? (
+                {!!isHeader ? (
+                    <CardHeader
+                        className={`${CardStyle.card_header} rounded-0`}
+                        {...(titleBg ? { style: { background: titleBg } } : "")}>
+                        <div
+                            className={`d-flex align-items-center ${
+                                icons.length > 0 ? "justify-content-between" : ""
+                            }`}>
+                            <div>
+                                {titleIcon ? (
+                                    <span className={classNames(styles.titleIcon, titleIcon)} />
+                                ) : null}
+                                <strong className={CardStyle.card_title}>{title}</strong>
+                                {subscript}
+                            </div>
+                            {icons.length > 0 && (
+                                <div className={`d-flex align-items-center ${CardStyle.card_icon}`}>
+                                    {icons?.map((item, index) => (
+                                        <Fragment key={index}>
                                             <button
                                                 type="button"
-                                                onClick={onClose}
+                                                onClick={() => {
+                                                    setIsOpen(
+                                                        item.icon === "fa fa-cog"
+                                                            ? !isOpen
+                                                            : "" || item.icon === "fa fa-ellipsis-v"
+                                                            ? !isOpen
+                                                            : ""
+                                                    );
+                                                }}
                                                 className={`${CardStyle.content_settings} border-0 bg-transparent`}>
-                                                <i className={"fa fa-times"} />
+                                                <i className={item.icon} />
                                             </button>
-                                        ) : null}
-                                    </div>
-                                )}
-                            </div>
-                        </CardHeader>
-                    ) : null}
 
-                    {headerPosition !== "bottom" ? (
-                        <div style={{ padding: padding, background: bodyBg }}>{children}</div>
-                    ) : null}
-                </BootstrapCard>
-            ) : null}
+                                            {isOpen && item?.dropdown?.length > 0 ? (
+                                                <div ref={ref} className={CardStyle.card_setting}>
+                                                    {item?.dropdown?.map((menu, i) => (
+                                                        <button
+                                                            key={i}
+                                                            type="button"
+                                                            onClick={menu?.method}>
+                                                            <i className={menu?.icon} />
+                                                            {menu.label}
+                                                        </button>
+                                                    ))}
+                                                </div>
+                                            ) : null}
+                                        </Fragment>
+                                    ))}
+                                    {!!dismissible ? (
+                                        <button
+                                            type="button"
+                                            onClick={onClose}
+                                            className={`${CardStyle.content_settings} border-0 bg-transparent`}>
+                                            <i className={"fa fa-times"} />
+                                        </button>
+                                    ) : null}
+                                </div>
+                            )}
+                        </div>
+                    </CardHeader>
+                ) : null}
+
+                {headerPosition !== "bottom" ? (
+                    <div style={{ padding: padding, background: bodyBg }}>{children}</div>
+                ) : null}
+            </BootstrapCard>
         </Fragment>
     );
 };
