deed62303d93f0d2f2ca48c650e60466129d015e
Dipok Roy <roydipok022@gmail.com>
2024-02-19 15:20:08 +0600

    monthly stats update

--- src/components/MonthlyStats/MonthlyStats.jsx
+++ src/components/MonthlyStats/MonthlyStats.jsx
@@ -1,92 +1,81 @@
-import { Card, CardHeader } from "react-bootstrap";
+import Card from "../Card/Card";
+import { CardBody } from "react-bootstrap";
 import { Line, LineChart, ResponsiveContainer } from "recharts";
-import { useDashboardDataContext } from "../../context/dashboardDataContext";
+import styles from "../../assets/scss/MonthlyStats.module.scss";
 
 const MonthlyStats = () => {
-    const { isDark } = useDashboardDataContext();
+    const data = [
+        {
+            name: "Page A",
+            uv: 400,
+            pv: 400,
+            amt: 2400,
+        },
+        {
+            name: "Page B",
+            uv: 30,
+            pv: 25000,
+            amt: 2210,
+        },
+        {
+            name: "Page C",
+            uv: 10,
+            pv: 10500,
+            amt: 2290,
+        },
+        {
+            name: "Page D",
+            uv: 20,
+            pv: 50000,
+            amt: 2000,
+        },
+        {
+            name: "Page A",
+            uv: 400,
+            pv: 400,
+            amt: 2400,
+        },
+        {
+            name: "Page B",
+            uv: 30,
+            pv: 25000,
+            amt: 2210,
+        },
+        {
+            name: "Page C",
+            uv: 10,
+            pv: 10500,
+            amt: 2290,
+        },
+        {
+            name: "Page D",
+            uv: 20,
+            pv: 50000,
+            amt: 2000,
+        },
+    ];
     return (
-        <Card
-            className="p-0 rounded-0 border-0"
-            style={{
-                padding: "20px",
-                backgroundColor: `${isDark ? "var(--bg-content)" : "#42a5f5"}`,
-                boxShadow: `${isDark ? "0 0 0 1px var(--border-color" : ""}`,
-            }}
-        >
-            <CardHeader className="rounded-0 bg-transparent border-0 d-flex align-items-center pb-3">
-                <h5
-                    className="text-white m-0 fw-semibold"
-                    style={{ fontSize: ".875em" }}
-                >
-                    February
-                </h5>
-            </CardHeader>
-            <Card.Body className="pt-0">
-                <ResponsiveContainer width="100%" height={85}>
-                    <LineChart
-                        width={250}
-                        height={82}
-                        data={[
-                            {
-                                name: "Page A",
-                                uv: 400,
-                                pv: 400,
-                                amt: 2400,
-                            },
-                            {
-                                name: "Page B",
-                                uv: 30,
-                                pv: 25000,
-                                amt: 2210,
-                            },
-                            {
-                                name: "Page C",
-                                uv: 10,
-                                pv: 10500,
-                                amt: 2290,
-                            },
-                            {
-                                name: "Page D",
-                                uv: 20,
-                                pv: 50000,
-                                amt: 2000,
-                            },
-                            {
-                                name: "Page A",
-                                uv: 400,
-                                pv: 400,
-                                amt: 2400,
-                            },
-                            {
-                                name: "Page B",
-                                uv: 30,
-                                pv: 25000,
-                                amt: 2210,
-                            },
-                            {
-                                name: "Page C",
-                                uv: 10,
-                                pv: 10500,
-                                amt: 2290,
-                            },
-                            {
-                                name: "Page D",
-                                uv: 20,
-                                pv: 50000,
-                                amt: 2000,
-                            },
-                        ]}
-                    >
-                        <Line
-                            type="monotone"
-                            dataKey="pv"
-                            stroke="#fff"
-                            strokeWidth={2}
-                        />
-                    </LineChart>
-                </ResponsiveContainer>
-            </Card.Body>
-        </Card>
+        <div className={styles.monthly_stats}>
+            <Card
+                title="February"
+                titleColor="#fff"
+                titleBg="#5c6bc0"
+                cardHeaderBorderColor="#5c6bc0"
+            >
+                <CardBody className={`${styles.card_body} pt-0`}>
+                    <ResponsiveContainer width="100%" height={85}>
+                        <LineChart width={250} height={82} data={data}>
+                            <Line
+                                type="monotone"
+                                dataKey="pv"
+                                stroke="#fff"
+                                strokeWidth={2}
+                            />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </CardBody>
+            </Card>
+        </div>
     );
 };
 
--- src/components/SocialStats/SocialStats.jsx
+++ src/components/SocialStats/SocialStats.jsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
-import { Card, CardBody, CardFooter, CardHeader } from "react-bootstrap";
+import Card from "../Card/Card";
+import { CardBody, CardFooter } from "react-bootstrap";
 import admin from "../../assets/image/admin.jpg";
-import style from "../../assets/scss/SocialStats.module.scss";
-import { useDashboardDataContext } from "../../context/dashboardDataContext";
+import styles from "../../assets/scss/SocialStats.module.scss";
 
 export const Stats = ({ icon, counter }) => {
     return (
-        <div className={`${style.stats} text-center`}>
+        <div className={`${styles.stats} text-center`}>
             <i className={icon}></i>
-            <span className={`${style.stats_counter} d-block fw-semibold`}>
+            <span className={`${styles.stats_counter} d-block fw-semibold`}>
                 {counter}
             </span>
         </div>
@@ -16,66 +15,63 @@ export const Stats = ({ icon, counter }) => {
 };
 
 const SocialStats = () => {
-    const [isOpen, setIsOpen] = useState();
-    const { isDark } = useDashboardDataContext();
     return (
-        <Card
-            className={`${style.card} border-0 rounded-0`}
-            style={{
-                boxShadow: `${isDark ? "0 0 0 1px var(--border-color)" : ""}`,
-            }}
-        >
-            <CardHeader
-                className={`${style.card_header} border-0 rounded-0 d-flex justify-content-end align-items-center`}
+        <div className={styles.social_Stats}>
+            <Card
+                titleBg="#5c6bc0"
+                cardHeaderBorderColor="#5c6bc0"
+                titleColor="#fff"
+                iconColor="#fff"
+                icons={[
+                    {
+                        icon: "fa fa-ellipsis-v",
+                        dropdown: [
+                            {
+                                label: "Edit",
+                                icon: "fa fa-cog",
+                                method: () => alert("Cog"),
+                            },
+                            {
+                                label: "Delete",
+                                icon: "fa-solid fa-trash",
+                                method: () => alert("Delete"),
+                            },
+                            {
+                                label: "Update",
+                                icon: "fa-solid fa-recycle",
+                                method: () => alert("Update"),
+                            },
+                        ],
+                    },
+                ]}
             >
-                <button
-                    type="button"
-                    className="border-0 bg-transparent text-white"
-                    onClick={() => setIsOpen(!isOpen)}
-                >
-                    <i className="fa fa-ellipsis-v"></i>
-                </button>
-                {isOpen ? (
-                    <div className={style.card_setting}>
-                        <button type="button">
-                            <i className="fa-solid fa-gear"></i>
-                            Edit
-                        </button>
-                        <button type="button">
-                            <i className="fa-solid fa-trash"></i>
-                            Delete
-                        </button>
-                        <button type="button">
-                            <i className="fa-solid fa-recycle"></i>
-                            Update
-                        </button>
+                <CardBody className="p-0">
+                    <div className={styles.user_info}>
+                        <h5 className="fs-6 fw-medium text-white text-center m-0">
+                            Martin Luther
+                        </h5>
                     </div>
-                ) : (
-                    ""
-                )}
-            </CardHeader>
-            <CardBody className={`${style.card_body} p-0`}>
-                <div className={style.user_info}>
-                    <h5 className="fs-6 fw-medium text-white text-center">
-                        Martin Luther
-                    </h5>
-                </div>
-                <div
-                    className={`${style.image_continer} d-flex justify-content-center align-items-center`}
-                >
-                    <img className="rounded-circle" src={admin} alt="user" />
-                </div>
-            </CardBody>
-            <CardFooter className={`${style.card_footer} p-3 py-2`}>
-                <div className="d-flex justify-content-between align-items-center">
-                    <Stats icon="fa-solid fa-thumbs-up" counter={3548} />
-                    <hr className="mx-3" />
-                    <Stats icon="fa-solid fa-user-group" counter={23548} />
-                    <hr className="mx-3" />
-                    <Stats icon="fa-solid fa-message" counter={3548} />
-                </div>
-            </CardFooter>
-        </Card>
+                    <div
+                        className={`${styles.image_continer} d-flex justify-content-center align-items-center`}
+                    >
+                        <img
+                            className="rounded-circle"
+                            src={admin}
+                            alt="user"
+                        />
+                    </div>
+                </CardBody>
+                <CardFooter className={`${styles.card_footer} p-3 py-2`}>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <Stats icon="fa-solid fa-thumbs-up" counter={3548} />
+                        <hr className="mx-3" />
+                        <Stats icon="fa-solid fa-user-group" counter={23548} />
+                        <hr className="mx-3" />
+                        <Stats icon="fa-solid fa-message" counter={3548} />
+                    </div>
+                </CardFooter>
+            </Card>
+        </div>
     );
 };
 
