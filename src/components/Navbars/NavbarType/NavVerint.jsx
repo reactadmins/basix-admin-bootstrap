a237ca17dba3c22081cce576037b3f5f0c049f32
Dipok Roy <roydipok022@gmail.com>
2024-02-22 18:06:31 +0600

    update

---
+++ src/components/Navbars/NavbarType/NavVerint.jsx
@@ -0,0 +1,10 @@
+import styles from "@/NavVerint.mobule.scss";
+const NavVerint = () => {
+    return (
+        <div className={styles.navbar}>
+            <h1></h1>
+        </div>
+    );
+};
+
+export default NavVerint;
